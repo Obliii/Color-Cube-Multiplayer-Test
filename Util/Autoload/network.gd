5692a98d3c3f144957c89e94d7e5882224d8f427
Obli <obliotter@gmail.com>
2025-02-28 22:08:58 -0600

    Update network.gd

--- Util/Autoload/network.gd
+++ Util/Autoload/network.gd
@@ -1,1 +1,56 @@
 extends Node
+
+signal player_connected(peer_id, player_info)
+signal player_disconnected(peer_id)
+signal server_disconnected
+
+const PORT = 30001
+const DEFAULT_SERVER_IP = "127.0.0.1"
+const MAX_CONNECTIONS = 20
+
+# Player info for every player.
+var players = {}
+
+# The idea is to get the name of the player, and the color that they have selected.
+var player_info = {"name": "Name", "color": "selected_color"}
+var players_loaded = 0
+
+func _ready():
+	multiplayer.peer_connected.connect(_on_player_connected)
+	multiplayer.peer_disconnected.connect(_on_player_disconnected)
+	multiplayer.connected_to_server.connect(_on_connected_ok)
+	multiplayer.connection_failed.connect(_on_connected_fail)
+
+func join_game(address = ""):
+	if address.is_empty():
+		address = DEFAULT_SERVER_IP
+	var peer = ENetMultiplayerPeer.new()
+	var error = peer.create_client(address, PORT)
+	if error:
+		return error
+	multiplayer.multiplayer_peer = peer
+
+func create_game():
+	var peer = ENetMultiplayerPeer.new()
+	var error = peer.create_server(PORT, MAX_CONNECTIONS)
+	if error:
+		return error
+	multiplayer.multiplayer_peer = peer
+	
+	players[1] = player_info
+	player_connected.emit(1, player_info)
+
+func remove_multiplayer_peer():
+	multiplayer.multiplayer_peer = null
+
+func _on_player_connected():
+	pass
+
+func _on_player_disconnected():
+	pass	
+
+func _on_connected_ok():
+	pass
+
+func _on_connected_fail():
+	pass
